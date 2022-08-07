f7a087f5d439fe9e0d8591c12cb7004ddd359cbb
Esaian Mark <qwert11223381@gmail.com>
2022-08-07 14:10:18 +0300

    Draft of Server.
    Signed-off-by: Esaian Mark <qwert11223381@gmail.com>

--- core/server/Server.cpp
+++ core/server/Server.cpp
@@ -1,1 +1,51 @@
 #include "Server.hpp"
+
+// Reference:
+// https://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/tutorial/tutdaytime3.html
+// https://www.boost.org/doc/libs/1_78_0/doc/html/boost_asio/example/cpp11/http/server/server.hpp
+// https://www.boost.org/doc/libs/1_78_0/doc/html/boost_asio/example/cpp11/http/server/server.cpp
+
+namespace tcp {
+    using TCP = boost::asio::ip::tcp;
+
+    Server::Server(std::string_view
+                   const &address,
+                   std::string_view const &port
+    ) : io_context_(1),
+        acceptor_(io_context_, TCP::endpoint(TCP::endpoint(TCP::v4(),
+                                                           8080))) {
+
+        boost::asio::ip::tcp::resolver resolver(io_context_);
+        boost::asio::ip::tcp::endpoint endpoint = *resolver.resolve(
+                address, port
+        ).begin();
+
+        acceptor_.open(endpoint.protocol());
+        acceptor_.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
+        acceptor_.bind(endpoint);
+        acceptor_.listen();
+
+        do_accept();
+    }
+
+    void Server::do_await_stop() {
+
+    }
+
+    void Server::do_accept() {
+        acceptor_.async_accept(
+                [this](boost::system::error_code ec,
+                       boost::asio::ip::tcp::socket socket) {
+                    if (!acceptor_.is_open()) {
+                        return;
+                    }
+
+                    if (!ec) {
+
+                    }
+
+                    do_accept();
+                }
+        );
+    }
+}
--- core/server/Server.hpp
+++ core/server/Server.hpp
@@ -1,6 +1,24 @@
 #pragma once
+
+#include <string>
+
 #include <boost/asio.hpp>
 
-class Server {
+namespace tcp {
+    class Server {
+    public:
+        Server(std::string_view const &address,
+               std::string_view const &port);
 
-};
+        void run();
+
+    private:
+        void do_accept();
+
+        void do_await_stop();
+
+        boost::asio::io_context io_context_;
+        boost::asio::ip::tcp::acceptor acceptor_;
+
+    };
+}
