461bd78c1dd585fcc9913907c3dd208928216e8f
Esaian Mark <qwert11223381@gmail.com>
2022-08-07 03:48:00 +0300

    Add clang-format and format files.
    Signed-off-by: Esaian Mark <qwert11223381@gmail.com>

--- include/wallet/wallet.h
+++ include/wallet/wallet.h
@@ -4,30 +4,30 @@
 extern "C" {
 #endif
 
-    enum BalanceAsset{
-        RUBBLES,
-        DOLLARS,
-        ETH,
-        BTC,
-        XSTUSD,
-    };
+enum BalanceAsset {
+    RUBBLES,
+    DOLLARS,
+    ETH,
+    BTC,
+    XSTUSD,
+};
 
-    enum Errors{
-        WALLET_OK = 0,
-        SERVER_ERROR,
-        BUFFER_ERROR,
-    };
+enum Errors {
+    WALLET_OK = 0,
+    SERVER_ERROR,
+    BUFFER_ERROR,
+};
 
-    Errors add_balance(BalanceAsset, double add);
-    Errors set_balance(BalanceAsset, double set);
+Errors add_balance(BalanceAsset, double add);
+Errors set_balance(BalanceAsset, double set);
 
-    /*
-     * Usage:
-     * get_all_relative_balance(BalanceAsset, double*,
-     *                          BalanceAsset, double*,
-     *                          ...);
-     */
-    Errors get_balance(...);
+/*
+ * Usage:
+ * get_all_relative_balance(BalanceAsset, double*,
+ *                          BalanceAsset, double*,
+ *                          ...);
+ */
+Errors get_balance(...);
 
 #ifdef __cplusplus
 }
