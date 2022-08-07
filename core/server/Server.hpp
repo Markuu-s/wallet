b650c9ee7b3f4c691b19534f39618d8940fed5c9
Esaian Mark <qwert11223381@gmail.com>
2022-08-07 18:38:57 +0300

    Add json-rpc library.
    Signed-off-by: Esaian Mark <qwert11223381@gmail.com>

--- core/server/Server.hpp
+++ core/server/Server.hpp
@@ -3,6 +3,26 @@
 #include <string>
 
 #include <boost/asio.hpp>
+#include <jsonrpccxx/server.hpp>
+
+/**
+ * JSON-BYTES:
+ *
+ *      Json-request-RPC:
+ *          {
+ *              "jsonrpc": "2.0": STRING,
+ *              "method": "${method_name}": STRING,
+ *              "params": {${}: ANY}: ARRAY,
+ *              "id": ${NUMBER_ID}: INT
+ *          }
+ *
+ *      Json-response-RPC:
+ *          {
+ *              "jsonrpc": "2.0": STRING,
+ *              "result": ${}: ANY,
+ *              "id": ${NUMBER_ID}: INT
+ *          }
+ */
 
 namespace tcp {
   class Server {
